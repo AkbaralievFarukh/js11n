45f4b36abf26cd02504033a8167a2d8d96ead69c
Farukh <akbfarukh@gmail.com>
2023-11-08 13:12:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const row = document.querySelector('.row')
+const all = document.querySelector('#all')
+const search = document.querySelector('#search')
+const searchInput = document.querySelector('#searchInput')
+const submit = document.querySelector('#submit')
+const searchWrapper = document.querySelector('.search-wrapper')
+const currentCountry = document.querySelector('.currentCountry')
+const apiKey = 'cc0297641400496380760659230811'
+
+all.addEventListener('change', () => {
+    if (all.checked) {
+        row.classList.remove('hidden')
+        searchWrapper.classList.add('hidden')
+        currentCountry.classList.add('hidden')
+    }
+})
+
+search.addEventListener('change', () => {
+    if (search.checked) {
+        row.classList.add('hidden')
+        searchWrapper.classList.remove('hidden')
+    }
+})
+
+
+const handleGetCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(country => {
+            row.innerHTML += `
+                <div class="col-4">
+                    <div class="card">
+                        <img src="${country.flags.png}" alt="">
+                        <div class="card-body">
+                            <h3 class="card-title">${country.translations.rus.official}</h3>
+                            <p class="card-text">${country.capital}</p>
+                        </div>
+                    </div>  
+                </div>
+            `
+        })
+    })
+}
+
+handleGetCountries()
+
+
+submit.addEventListener('click', () => {
+    currentCountry.classList.remove('hidden')
+    const handleGetNameCountries = () => {
+        let nameCountry = searchInput.value
+        console.log(nameCountry)
+        fetch(`https://restcountries.com/v3.1/name/${nameCountry}`)
+        .then(response => response.json())
+        .then(data => {
+            const result = data[0]
+            const capitalCity = result.capital;
+            fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${capitalCity}`)
+                .then(weatherResponse => weatherResponse.json())
+                .then(weatherData => {
+                    const currency = Object.values(result.currencies)
+                    currentCountry.innerHTML += `
+                        <div class="col-4">
+                            <div class="card">
+                                <img src="${result.flags.png}" alt="">
+                                <div class="card-body">
+                                    <h3 class="card-title">${result.name.official}</h3>
+                                    <p class="card-text">${result.capital}</p>
+                                    <p class="card-text">${weatherData.current.temp_c}°C</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                });
+        })
+    }
+    handleGetNameCountries()
+})
